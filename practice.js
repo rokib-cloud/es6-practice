f53a74f84a063128ded8e99427e6131479142c35
rokib-cloud <roibh393@gmail.com>
2021-06-09 15:53:01 +0600

    addedsome of files

---
+++ practice.js
@@ -0,0 +1,13 @@
+function createUser(name=rokib,age=18){
+
+    const user = {createUser};
+    
+    user.name = name;
+    // console.log(user.name)
+    user.age = age;
+    
+    return user;
+    
+    }
+    
+    
---
+++ template.js
@@ -0,0 +1,19 @@
+// es6 templates
+const firstName = "ROKIB";
+const lastName = "HOSSAIN";
+const fullName = firstName + " " + lastName + "is a programmer";
+const fullName2 = `${firstName} ${lastName} is a good boy`;
+console.log(fullName2);
+
+
+const multiline = "I want to be a programmer\n"
+    + "I need a bike\n"
+    + "i need a car";
+console.log(multiline);
+// es6
+const multiline2 = `I need bike
+I need a car
+I need money
+but it should be halal`;
+console.log(multiline2)
+
