56f2ef80de7f650efe132cb6feecf8494044e21c
rokib-cloud <roibh393@gmail.com>
2021-06-09 14:56:22 +0600

    added lestconst.js

---
+++ letconst.js
@@ -0,0 +1,19 @@
+const wife = "Tassen Shammi";
+// wife ="Annie Marry";
+console.log(wife);
+
+const numbers = [56, 64];
+numbers[2] = 43;
+numbers.push(14);
+const doctor = { name: 'Musfiq', time: 6.00 }
+// console.log(numbers);
+
+let patientName = 'Badshah Miah';
+patientName ='Monoara Begum';
+console.log(patientName);
+
+let sum = 0;
+for(let i = 0; i < 10; i++){
+    sum = sum + i;
+}
+console.log(sum);
