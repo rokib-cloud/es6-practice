88f2f50d24440c7a1d06d4ed4d94b03bf9c5e2c4
rokib-cloud <roibh393@gmail.com>
2021-06-09 16:50:11 +0600

    added arrow.js

---
+++ arrow.js
@@ -0,0 +1,31 @@
+// function doubleIt(num){
+//     return num * 2;
+// }
+
+
+
+// const doubleIt = function myFun(num){
+//     return num * 2;
+// }
+
+
+// const doubleIt = num => num * 3;
+// const add =(x, y) => x + y;
+// const give5 = () => 5;
+
+
+
+
+// const result = add(50, 56);
+// const result2 = give5();
+
+// console.log(result);
+
+const doMath = (x, y) => {
+    const sum = x + y;
+    const diff = x - y;
+    const result = sum * diff ;
+    return result;
+}
+const result3 = doMath(7, 5);
+console.log(result3)
